12524aca892f0c63c0070efad8528d7f63979fbb
Will Ceolin <wceolin@users.noreply.github.com>
2023-11-08 19:05:55 -0300

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,19 @@
+import type { Viewport, Metadata } from "next";
+
 import "./globals.css";
 
 type RootLayoutProps = {
   children: React.ReactNode;
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FACC15",
+  colorScheme: "light",
+};
+
+export const metadata: Metadata = {
+  manifest: "/site.webmanifest",
+  category: "education",
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "...",
-  description: "...",
+  title: "UneeBee | Open-source interactive courses",
+  description:
+    "UneeBee is an open-source alternative to Duolingo. Easily create interactive courses.",
+  twitter: {
+    title: "UneeBee",
+    description: "An open-source, white-label alternative to Duolingo.",
+    creator: "@zoonkeducation",
+  },
 };
 
 export default function Home() {
