50d1a736997207cc119588348c1921eb048113d8
Will Ceolin <wceolin@users.noreply.github.com>
2023-11-18 15:50:18 -0300

    Features: Remove unnecessary containers

--- components/EditorFeatures.tsx
+++ components/EditorFeatures.tsx
@@ -26,54 +26,48 @@ const features = [
 
 export default function EditorFeatures() {
   return (
-    <div className="overflow-hidden mt-4 bg-indigo-50 rounded-lg py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-8">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">
-                Teach faster
-              </h2>
+    <div className="overflow-hidden mx-auto grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 max-w-7xl mt-4 px-6 lg:px-8 bg-indigo-50 rounded-lg py-24 sm:py-32">
+      <div className="lg:pr-8 lg:max-w-lg">
+        <h2 className="text-base font-semibold leading-7 text-indigo-600">
+          Teach faster
+        </h2>
 
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Effortless Content Creation
-              </p>
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Effortless Content Creation
+        </p>
 
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                UneeBee lets you quickly build lessons that go beyond watching
-                videos. Bring your content to life with real-world scenarios,
-                making it easy to create content that’s interactive and
-                impactful. Lead your audience through choices that matter,
-                fostering a deeper understanding with each step.
-              </p>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          UneeBee lets you quickly build lessons that go beyond watching videos.
+          Bring your content to life with real-world scenarios, making it easy
+          to create content that’s interactive and impactful. Lead your audience
+          through choices that matter, fostering a deeper understanding with
+          each step.
+        </p>
 
-              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                {features.map((feature) => (
-                  <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-gray-900">
-                      <feature.icon
-                        className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
-                        aria-hidden="true"
-                      />
-                      {feature.name}
-                    </dt>{" "}
-                    <dd className="inline">{feature.description}</dd>
-                  </div>
-                ))}
-              </dl>
+        <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative pl-9">
+              <dt className="inline font-semibold text-gray-900">
+                <feature.icon
+                  className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                  aria-hidden="true"
+                />
+                {feature.name}
+              </dt>{" "}
+              <dd className="inline">{feature.description}</dd>
             </div>
-          </div>
-
-          <Image
-            src={editor}
-            alt="Lesson editor screenshot"
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 md:-ml-4 lg:-ml-0"
-            width={1024}
-            height={1149}
-            unoptimized
-          />
-        </div>
+          ))}
+        </dl>
       </div>
+
+      <Image
+        src={editor}
+        alt="Lesson editor screenshot"
+        className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 md:-ml-4 lg:-ml-0"
+        width={1024}
+        height={1149}
+        unoptimized
+      />
     </div>
   );
 }
--- components/PlayerFeatures.tsx
+++ components/PlayerFeatures.tsx
@@ -26,53 +26,47 @@ const features = [
 
 export default function PlayerFeatures() {
   return (
-    <div className="overflow-hidden bg-slate-50 rounded-lg py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-8">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">
-                Better learning
-              </h2>
+    <div className="overflow-hidden mx-auto grid  grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 max-w-7xl px-6 lg:px-8 bg-slate-50 rounded-lg py-24 sm:py-32">
+      <div className="lg:pr-8 lg:max-w-lg">
+        <h2 className="text-base font-semibold leading-7 text-indigo-600">
+          Better learning
+        </h2>
 
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Interactive Lessons
-              </p>
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Interactive Lessons
+        </p>
 
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Use the power of learning by doing with interactive lessons. Our
-                platform brings scenarios to life, allowing learners to engage
-                with content, make decisions, and see real-time consequences of
-                their choices.
-              </p>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Use the power of learning by doing with interactive lessons. Our
+          platform brings scenarios to life, allowing learners to engage with
+          content, make decisions, and see real-time consequences of their
+          choices.
+        </p>
 
-              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                {features.map((feature) => (
-                  <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-gray-900">
-                      <feature.icon
-                        className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
-                        aria-hidden="true"
-                      />
-                      {feature.name}
-                    </dt>{" "}
-                    <dd className="inline">{feature.description}</dd>
-                  </div>
-                ))}
-              </dl>
+        <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative pl-9">
+              <dt className="inline font-semibold text-gray-900">
+                <feature.icon
+                  className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                  aria-hidden="true"
+                />
+                {feature.name}
+              </dt>{" "}
+              <dd className="inline">{feature.description}</dd>
             </div>
-          </div>
-
-          <Image
-            src={playImage}
-            alt="Screenshot of the course list"
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-            width={1024}
-            height={805}
-            unoptimized
-          />
-        </div>
+          ))}
+        </dl>
       </div>
+
+      <Image
+        src={playImage}
+        alt="Screenshot of the course list"
+        className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+        width={1024}
+        height={805}
+        unoptimized
+      />
     </div>
   );
 }
